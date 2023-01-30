aefaef08a6ca4421003f37d548418be0e4882345
Dzmitry Ramantsevich <d.ramantsevich@gmail.com>
2023-01-30 21:27:20 +0300

    Lesson 9 - HTML forms(@ModelAttribute annotation)

--- src/main/java/org/example/controllers/PeopleController.java
+++ src/main/java/org/example/controllers/PeopleController.java
@@ -1,12 +1,11 @@
 package org.example.controllers;
 
 import org.example.daos.PersonDAO;
+import org.example.models.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/people")
@@ -30,5 +29,17 @@ public class PeopleController {
         model.addAttribute("people", personDAO.getPersonById(id));
 
         return "people/people";
+    }
+
+    @GetMapping("/new")
+    public String newPerson(@ModelAttribute("person") Person person) {
+        return "people/new";
+    }
+
+    @PostMapping()
+    public String create(@ModelAttribute("person") Person person) {
+        personDAO.save(person);
+
+        return "redirect:/people";
     }
 }
